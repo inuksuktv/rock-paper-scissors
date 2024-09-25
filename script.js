a7c326a57cc66dfb00e72d91afa4df7e1187e2fb
Bradley Morin <bradleymorin@gmail.com>
2024-09-24 23:20:51 -0600

    Add function to get the computer's choice.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber < 0.333) {
+        return "rock";
+    }
+    if (randomNumber < 0.666) {
+        return "paper";
+    }
+    return "scissors";
+}
