42ec9b75dfc7556d235ef06fd827d5869fcc2928
Bradley Morin <bradleymorin@gmail.com>
2024-09-24 23:54:39 -0600

    Add humanChoice function to get the player's input

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function getComputerChoice() {
     }
     return "scissors";
 }
+
+function getHumanChoice() {
+    let choice = prompt("Enter rock, paper, or scissors.")
+    choice = choice.toLowerCase();
+    if (choice == "rock") {
+        return "rock";
+    }
+    if (choice == "paper") {
+        return "paper";
+    }
+    if (choice == "scissors") {
+        return "scissors";
+    }
+    console.log("Invalid selection. Try again.")
+    getHumanChoice();
+}
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
